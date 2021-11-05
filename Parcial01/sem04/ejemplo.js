3a077fe1036587167ef4de5c847a2a8f39547c1a
Eddy Espinoza <josujrth@gmail.com>
2021-11-04 19:47:45 -0500

    correcion de codigo e instalacion de algunas librerias

--- Parcial01/sem04/ejemplo.js
+++ Parcial01/sem04/ejemplo.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const conexion = `mongodb+srv://alberto:eddybsc123@cluster0.gdjrq.mongodb.net/practica?retryWrites=true&w=majority`;
 
 
+(async ()=>{
+  await mongoose.connect(conexion,  { useNewUrlParser:true, useUnifiedTopology:true });
+  const Usuario =  mongoose.model("Usuario", { nombre:String });
+  const usuario1 = new Usuario({nombre:"Evelyn Valdez"});
+  const guardo = await usuario1.save();
+  const respuesta = await Usuario.find();
+  console.log(respuesta);
+})();
 
-
-mongoose.connect(conexion,  { useNewUrlParser:true, useUnifiedTopology:true });
-
-const Usuario =  mongoose.model("Usuario", { nombre:String });
-const usuario1 = new Usuario({nombre:"Evelyn Valdez"});
-
-usuario1.save();
-Usuario.find().then(console.log)
